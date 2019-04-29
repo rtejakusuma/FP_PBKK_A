d7dbe36de81e1e94fecd3b84fbf846864f746636
hafid <hfdbch@gmail.com>
2019-04-29 19:08:03 +0700

    add tour list

--- src/com/mangujang/piknikYuk/controller/TourController.java
+++ src/com/mangujang/piknikYuk/controller/TourController.java
@@ -1,25 +1,35 @@
 package com.mangujang.piknikYuk.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mangujang.piknikYuk.model.Tour;
+import com.mangujang.piknikYuk.service.TourService;
+
 @Controller
 @RequestMapping("/tour")
 public class TourController {
+	
+	// inject tourService
+	@Autowired
+	private TourService tourService;
 	
 	@GetMapping("/list")
 	public String listTour(Model theModel) {
 		
 		// inject tourService
+		List<Tour> theTours = tourService.getTours();
 		
 		// add tour to theModel
+		theModel.addAttribute("tours", theTours);
 		
 		// load jsp page
-		
-		
-		return null;
+		return "tour/list-tour";
 	}
 
 }
--- src/com/mangujang/piknikYuk/dao/UserDAO.java
+++ src/com/mangujang/piknikYuk/dao/UserDAO.java
@@ -5,6 +5,5 @@ import java.util.List;
 import com.mangujang.piknikYuk.model.User;
 
 public interface UserDAO {
-	
 	public List<User> getUsers();
 }
