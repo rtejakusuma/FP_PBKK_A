08c4bef45dd6a3f7cc0d2a561a379517b757c75c
RavenRindou <rtejakusuma21@yahoo.com>
2019-04-29 21:12:09 +0700

    init class discount

---
+++ src/com/mangujang/piknikYuk/model/Discounts.java
@@ -0,0 +1,88 @@
+package com.mangujang.piknikYuk.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="discounts")
+public class Discounts {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="discount_value")
+	private double discountValue;
+	
+	@Column(name="start_time")
+	@Temporal(TemporalType.DATE)
+	private Date startTime;
+	
+	@Column(name="end_time")
+	@Temporal(TemporalType.DATE)
+	private Date endTime;
+
+	public Discounts() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getDiscountValue() {
+		return discountValue;
+	}
+
+	public void setDiscountValue(double discountValue) {
+		this.discountValue = discountValue;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Discounts [id=" + id + ", description=" + description + ", discountValue=" + discountValue
+				+ ", startTime=" + startTime + ", endTime=" + endTime + "]";
+	}
+	
+	
+}
